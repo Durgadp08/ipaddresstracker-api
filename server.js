26085d3fce733fa50b8e78cf9f332c08be21bfa8
Durga Prasad <durgaamarapalli0801@gmail.com>
2023-03-13 22:00:06 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const http = require('http')
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+let ip = '';
+app.get("/", (req, res) => {
+  http.get(`http://ip-api.com/json/${ip}`,response=>{
+    response.on('data',data=>{
+      res.json(JSON.parse(data))
+    })
+    response.on('error',err=>{
+      console.error(err);
+    })
+  })
+});
+
+app.post("/", (req, res) => {
+  const data = req.body;
+  console.log(data)
+  ip=data.input;
+  res.redirect('/')
+});
+
+app.listen("5000", () => {
+  console.log("Server Started at port 5000");
+});
